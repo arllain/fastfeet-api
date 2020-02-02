cdb30fa9c541d8741b1aeb2c7b3165321db89305
arllainm <arllain@gmail.com>
2020-02-01 21:20:41 -0300

    Middleware authentication

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -5,6 +5,9 @@ class UserController {
   async show(req, res) {
     const { id } = req.params;
     const user = await User.findOne({ where: { id } });
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
 
     return res.json(user);
   }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.get('/users/login', SessionController.login);
 routes.get('/users/:id', UserController.show);
+
+routes.use(authMiddleware);
+
 export default routes;
