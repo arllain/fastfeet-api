1e08c35fff020c5cade2b403edb1dc8c092506a3
arllainm <arllain@gmail.com>
2020-06-09 00:34:35 -0300

    changed database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,29 +1,48 @@
 require('dotenv/config');
 
-console.log(`process.env.DB_USER = ${process.env.DB_USER}`);
-console.log(`process.env.DB_PASS = ${process.env.DB_PASS}`);
-console.log(`process.env.DB_HOST = ${process.env.DB_HOST}`);
-console.log(`process.env.DB_PORT = ${process.env.DB_PORT}`);
-console.log(`process.env.DB_NAME = ${process.env.DB_NAME}`);
-
 module.exports = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  dialect: 'postgres',
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  development: {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
 
-  // cli: {
-  //   migrationsDir: ['src/database/migrations/'],
-  //   entitiesDir: 'src/app/models',
-  // },
-
-  define: {
-    timestamp: true,
-    underscored: true,
-    underscoredAll: true,
+    define: {
+      timestamp: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  test: {
+    username: process.env.PGUSER || 'root',
+    password: null,
+    database: 'react_webpack_node_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
   },
 };
+
+// module.exports = {
+//   type: 'postgres',
+//   url: process.env.DATABASE_URL,
+//   dialect: 'postgres',
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+
+//   define: {
+//     timestamp: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// };
