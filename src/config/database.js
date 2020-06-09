01621316049dd93441d943c21bdfda7cf59de86c
arllainm <arllain@gmail.com>
2020-06-08 22:55:58 -0300

    Heroku config

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,19 @@
 require('dotenv/config');
 
 module.exports = {
+  // type: 'postgres',
+  // url: process.env.DATABASE_URL,
   dialect: 'postgres',
   host: 'localhost',
   port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+
+  // cli: {
+  //   migrationsDir: ['src/database/migrations/'],
+  //   entitiesDir: 'src/app/models',
+  // },
 
   define: {
     timestamp: true,
