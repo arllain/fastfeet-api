a229d93201d297f7598c8bbde857c39f7826932c
arllainm <arllain@gmail.com>
2020-06-09 00:03:05 -0300

    databese configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,19 @@
 require('dotenv/config');
 
+console.log(`process.env.DB_USER = ${process.env.DB_USER}`);
+console.log(`process.env.DB_PASS = ${process.env.DB_PASS}`);
+console.log(`process.env.DB_HOST = ${process.env.DB_HOST}`);
+console.log(`process.env.DB_PORT = ${process.env.DB_PORT}`);
+console.log(`process.env.DB_NAME = ${process.env.DB_NAME}`);
+
 module.exports = {
-  // type: 'postgres',
+  type: 'postgres',
   // url: process.env.DATABASE_URL,
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
 
   // cli: {
