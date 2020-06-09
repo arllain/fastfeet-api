85acbd37495963154aad99feea533790ef22f9ea
arllainm <arllain@gmail.com>
2020-06-09 00:50:31 -0300

    databese

--- src/config/database.js
+++ src/config/database.js
@@ -1,45 +1,23 @@
 require('dotenv/config');
 
 module.exports = {
-  development: {
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  dialect: 'postgres',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 
-    define: {
-      timestamp: true,
-      underscored: true,
-      underscoredAll: true,
-    },
-  },
-  production: {
-    dialect: 'postgres',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+  // cli: {
+  //   migrationsDir: ['src/database/migrations/'],
+  //   entitiesDir: 'src/app/models',
+  // },
+
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
   },
 };
-
-// module.exports = {
-//   type: 'postgres',
-//   url: process.env.DATABASE_URL,
-//   dialect: 'postgres',
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-
-//   define: {
-//     timestamp: true,
-//     underscored: true,
-//     underscoredAll: true,
-//   },
-// };
